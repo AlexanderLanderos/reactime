5bee329d7c2ef2883076b420bb4fd6c359bfd672
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-10-17 21:00:36 -0700

    correcting component map on branch zoomFeature

--- src/app/components/ComponentMap.tsx
+++ src/app/components/ComponentMap.tsx
@@ -12,13 +12,12 @@ import useForceUpdate from './useForceUpdate';
 import LinkControls from './LinkControls';
 import getLinkComponent from './getLinkComponent';
 
-// setting the base margins for the Map to render in the Chrome extension window.
-const defaultMargin = { top: 30, left: 30, right: 30, bottom: 30 };
+const defaultMargin = { top: 30, left: 30, right: 30, bottom: 70 };
 
-// export these types because this will only be used on this page, interface not needed as it will not be re-used.
 export type LinkTypesProps = {
   width: number;
   height: number;
+  events?: boolean;
   margin?: { top: number; right: number; bottom: number; left: number };
   snapshots: [];
 };
@@ -30,18 +29,15 @@ export default function ComponentMap({
   margin = defaultMargin,
   snapshots: snapshots,
 }: LinkTypesProps) {
-  console.log(totalHeight);
-
-  // This is where we select the last object in the snapshots array from props to allow hierarchy to parse the data for render on the component map per hierarchy layout specifications.
+  // preparing the data to be used for render
   const lastNode = snapshots.length - 1;
   const data = snapshots[lastNode];
-  // importing custom hooks for the selection tabs.
   const [layout, setLayout] = useState<string>('cartesian');
   const [orientation, setOrientation] = useState<string>('horizontal');
   const [linkType, setLinkType] = useState<string>('diagonal');
-  const [stepPercent, setStepPercent] = useState<number>(10);
+  const [stepPercent, setStepPercent] = useState<number>(0.5);
   const forceUpdate = useForceUpdate();
-
+  // setting the margins for the Map to render in the tab
   const innerWidth = totalWidth - margin.left - margin.right;
   const innerHeight = totalHeight - margin.top - margin.bottom;
 
@@ -49,8 +45,7 @@ export default function ComponentMap({
   let sizeWidth: number;
   let sizeHeight: number;
 
-  // Conditional statement sets the location of the root node in the middle of the window
-  // Else statement sets the location of the root node to the right or top of the window per dropdown selection.
+  // rendering for the different tab selections
   if (layout === 'polar') {
     origin = {
       x: innerWidth / 2,
@@ -68,12 +63,7 @@ export default function ComponentMap({
       sizeHeight = innerWidth;
     }
   }
-
-  // render controls for the map
-  // svg - complete layout of self contained component map
-  // Tree is rendering each component from the component tree.
-  // rect- Contains both text and rectangle node information for rendering each component on the map.
-  // circle- setup and layout for the root node.
+  // controls for the map
   const LinkComponent = getLinkComponent({ layout, linkType, orientation });
   return totalWidth < 10 ? null : (
     <div>
@@ -95,7 +85,7 @@ export default function ComponentMap({
           <Tree
             root={hierarchy(data, (d) => (d.isExpanded ? null : d.children))}
             size={[sizeWidth, sizeHeight]}
-            separation={(a, b) => (a.parent === b.parent ? 10 : 0) / a.depth}
+            separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
           >
             {(tree) => (
               <Group top={origin.y} left={origin.x}>
@@ -109,7 +99,7 @@ export default function ComponentMap({
                     fill='none'
                   />
                 ))}
-                translate
+
                 {tree.descendants().map((node, key) => {
                   const width = 40;
                   const height = 15;
@@ -154,7 +144,6 @@ export default function ComponentMap({
                           rx={node.data.children ? 0 : 10}
                           onClick={() => {
                             node.data.isExpanded = !node.data.isExpanded;
-
                             forceUpdate();
                           }}
                         />
